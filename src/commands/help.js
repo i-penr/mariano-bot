debf351b68fb80ca8b2159149654749c8c8ea442
Zatapo <peterlapatatita@gmail.com>
2020-02-18 20:47:14 +0100

    Debug for help cmd

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,25 +11,30 @@ module.exports = {
 
   execute(msg, args, client) {
 
-    let page = args[1];
+    let page;
     const MAX = 10;
+    const maxPage = Math.ceil(commandFiles.length / MAX);
 
-    if(!page) {
+    if (isNaN(args[1]) || !args[1]) {
       page = 1;
+    } else { page = Number(args[1]); }
+
+    if (page > maxPage) {
+      page = maxPage;
+      console.log(page);
     }
     page--;
 
     const embed = new Discord.RichEmbed()
-
       .setColor('#0099ff')
       .setTitle('Command list')
       .setAuthor('Mariano', client.user.avatarURL)
       /* .setThumbnail('https://i.imgur.com/wSTFkRM.png')
       .setImage('https://i.imgur.com/wSTFkRM.png') */
       .setTimestamp()
-      .setFooter(`Page ${page + 1}/${Math.ceil(commandFiles.length / MAX)}`);
+      .setFooter(`Page ${page + 1}/${maxPage}`);
 
-    for (let i = page * 3; i < 3 * page + 3; i++) {  // Sustituir 3 por MAX cuando haya más comandos
+    for (let i = page * commandFiles.length; i < commandFiles.length * page + commandFiles.length; i++) { // Sustituir commandFiles.length por MAX cuando haya más comandos
       commandFiles.sort();
       const command = require(`./${commandFiles[i]}`);
       embed.addField(command.name, command.description, false);
